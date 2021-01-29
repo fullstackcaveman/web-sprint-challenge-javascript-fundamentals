4c1c8a5ce90489a10c8d7fe56f6d892abf0e2bb1
Chris Burton <chris@fullstackcaveman.com>
2021-01-29 10:08:30 -0700

    Solved Callbacks - Step:3

--- index.js
+++ index.js
@@ -194,8 +194,8 @@ function add(num1, num2) {
 
 // 🦁🦁🦁 Use multiply to return the product of two numbers 🦁🦁🦁
 
-function multiply(/*Your Code Here */) {
-	/*Your Code Here */
+function multiply(num1, num2) {
+	return num1 * num2;
 }
 
 // 🦁🦁🦁 Use greeting to accept a first and last name and return "Hello {first-name} {last-name}, nice to meet you!" 🦁🦁🦁
